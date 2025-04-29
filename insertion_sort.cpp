2906978561abe05f72e94b50719616d480e6940e
Ommane123 <ommane1327@gmail.com>
2025-04-29 13:20:44 +0530

    b

--- insertion_sort.cpp
+++ insertion_sort.cpp
@@ -3,8 +3,18 @@
 using namespace std;
 
 int insertionSort(vector<int> &v)
+{
+    int n = v.size();
+    for (int i = 1; i < n; i++)
+    {
+        int current_index = i;
+        for (int j = i + 1; j < n; j++)
+        {
+        }
+    }
+}
 
-    int main()
+int main()
 {
 
     return;

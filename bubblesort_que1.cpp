7f9dd7159fdb3f15a582948b05e63d2ad15b4695
Ommane123 <ommane1327@gmail.com>
2025-05-03 18:10:24 +0530

    q

--- bubblesort_que1.cpp
+++ bubblesort_que1.cpp
@@ -1,60 +1,8 @@
 #include <iostream>
 #include <vector>
-
 using namespace std;
-
-void bubbleSort(vector<int> &v)
-{
-    int n = v.size();
-
-    for (int i = 0; i < n - 1; i++)
-    {
-        bool flag = false;
-        for (int j = 0; j < n - i - 1; j++)
-        {
-            if (v[j] > v[j + 1])
-            {
-                flag = true;
-                swap(v[j], v[j + 1]);
-            }
-        }
-        if (!flag)
-            break;
-    }
-    return;
-}
-
 int main()
 {
 
-    cout << "Bubble Sort Algorithm" << endl;
-    cout << "Enter the size of array!" << endl;
-    int n;
-    cin >> n;
-
-    cout << "Enter " << n << " Numbers: " << endl;
-
-    vector<int> v(n);
-
-    for (int i = 0; i < n; i++)
-    {
-        cin >> v[i];
-    }
-
-    bubbleSort(v);
-
-    for (int i = 0; i < n; i++)
-    {
-        cout << v[i] << " ";
-    }
-    cout << endl;
-
-    cout << "Note - In Bubble Sort repeatedly swap elements if they are in wrong order" << endl;
-
-    // The Maximum no of swaps in worst case in bubble sort is = n-1 + n - 2 + .....+1;
-    // which is n(n-1)/2
-
-    // Time complexity : depends on n(n-1)/2  therefore, = O(n^2) due to ignoring constant
-    // Space complexity : O(1)
-    // bubble sort is stable algo
+    return 0;
 }

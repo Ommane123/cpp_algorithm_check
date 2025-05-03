4451f0829b686611a8f53d4ac18390a12d1ceccf
Ommane123 <ommane1327@gmail.com>
2025-05-03 18:13:24 +0530

    new

--- bubblesort_que1.cpp
+++ bubblesort_que1.cpp
@@ -4,5 +4,22 @@ using namespace std;
 int main()
 {
 
+    int n;
+    cin >> n;
+
+    vector<int> v(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> v[i];
+    }
+
+    swap_zeros_to_end(v);
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << v[i] << " ";
+    }
+    cout << endl;
+
     return 0;
 }

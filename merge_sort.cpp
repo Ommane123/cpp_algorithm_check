b8e7c4705ce6af022770e474e304d77f80d8117f
Ommane123 <ommane1327@gmail.com>
2025-05-08 18:35:33 +0530

    initiate

--- merge_sort.cpp
+++ merge_sort.cpp
@@ -4,5 +4,27 @@ using namespace std;
 
 int main()
 {
+
+    int n;
+    cout << "Enter the size of array : " << endl;
+    cin >> n;
+
+    vector<int> v(n);
+
+    cout << "Enter elements of array : " << endl;
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> v[i];
+    }
+
+    for (int i = 0; i < n - 1; i++)
+    {
+        cout << v[i] << " ";
+    }
+    cout << endl;
+
     return 0;
 }
+
+// Time complexity = O(n^2/2)+4

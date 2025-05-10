41fa7a9178d7bb15337fc1775e2cfdb65f1b091a
Ommane123 <ommane1327@gmail.com>
2025-05-10 16:38:56 +0530

    sfdfs

--- merge.cpp
+++ merge.cpp
@@ -7,6 +7,18 @@ void merge(int arr[], int l, int mid, int r)
 
     int an = mid - l + 1;
     int bn = r - mid;
+    // create 2 temp arrays
+    int a[an];
+    int b[bn];
+
+    for (int i = 0; i < an; i++)
+    {
+        a[i] = arr[l + i];
+    }
+    for (int j = 0; j < bn; j++)
+    {
+        b[j] = arr[mid + 1 + j];
+    }
 }
 
 int main()

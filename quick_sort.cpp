21b3d85cfa712d6db140961552373da104545f8c
Ommane123 <ommane1327@gmail.com>
2025-05-10 19:26:53 +0530

    quick sort done

--- quick_sort.cpp
+++ quick_sort.cpp
@@ -2,8 +2,55 @@
 
 using namespace std;
 
+int partition(int arr[], int first, int last)
+{
+
+    int pivot = arr[last];
+    int i = first - 1; // for inserting ele lest than pivot
+    int j = first;     // for finding ele < pivot
+
+    for (; j < last; j++)
+    {
+        if (arr[j] < pivot)
+        {
+            i++;
+            swap(arr[i], arr[j]);
+        }
+    }
+    // now i is pointing to the last ele < pivot
+    //  correct position for pivot will be = i+1
+    swap(arr[i + 1], arr[last]);
+    return i + 1;
+}
+
+void quickSort(int arr[], int first, int last)
+{
+
+    // base case
+    if (first >= last)
+    {
+        return;
+    }
+
+    // recursive caase
+    int pi = partition(arr, first, last);
+    quickSort(arr, first, pi - 1);
+    quickSort(arr, pi + 1, last);
+}
+
 int main()
 {
 
-        return 0;
+    int arr[] = {20, 12, 35, 16, 18, 30};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    quickSort(arr, 0, n - 1);
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
 }

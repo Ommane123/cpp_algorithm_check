a738272ffcaf53186079009a0ca6fd90d948f702
Ommane123 <ommane1327@gmail.com>
2025-08-06 18:41:31 +0530

    space and time

--- revision.cpp
+++ revision.cpp
@@ -4,6 +4,27 @@ using namespace std;
 int main()
 {
     // the rate at which the time taken increases with respect to the input size == Time Complexity
+    // UNIT OF TIME COMPLEXITY == Big O
+    // Rules :
+    //      1. Tell worst case scenario
+    /*      2. Avoid constants
+            3. Avoid lower values
 
-        return 0;
+            e.g. - O(n^15 + n^10 + 3) here,   3 == constant
+                                              n^10 == lower constant
+            here we are gonna tell n^15 cause its the biggest value...
+
+            Time limit = 1s(second) = 10^8 in interview or in questions they might ask if its 3s = 3*10^8 or if 5s = 5*10^8
+
+
+Space Complexity :
+        Neverever manipulate input data
+        like, a = 5; b = 6;
+                b = a + b; this is wrong
+
+
+
+
+    */
+    return 0;
 }

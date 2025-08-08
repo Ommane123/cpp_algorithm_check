f9cfba97228404fa4a1d7f9148207688295e0bec
Ommane123 <ommane1327@gmail.com>
2025-08-08 18:29:45 +0530

    day3.1

--- Recursion.cpp
+++ Recursion.cpp
@@ -1,8 +1,22 @@
 #include <bits/stdc++.h>
 using namespace std;
+int n = 0;
+void recur()
+{
+
+    if (n == 4)
+        return;
+
+    cout << n << endl;
+    n++;
+    recur();
+}
 
 int main()
 {
 
+    // Recursion leads to stack overflow.
+
+    recur();
     return 0;
 }

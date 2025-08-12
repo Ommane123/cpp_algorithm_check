ec7576180b4709ab4d321b1c93c9ecf28608d2e6
Ommane123 <ommane1327@gmail.com>
2025-08-12 22:22:46 +0530

    day4.2

--- recursion1.cpp
+++ recursion1.cpp
@@ -1,7 +1,18 @@
 #include <bits/stdc++.h>
 using namespace std;
+
+int sumation(int i, int sum)
+{
+}
+
 int main()
 {
+    // sumation of 1st n numbers
+    int n;
+    cout << "Enter value of n : " << endl;
+    cin >> n;
+
+    sumation(n, 0);
 
     return 0;
 }
